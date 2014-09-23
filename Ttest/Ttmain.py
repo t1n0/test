519569cc18c0022e6599ca8fd47248d6810c7b67
t1n0 <tino.mehlmann@gmail.com>
2014-09-22 20:47:15 -0400

    Change 1

--- Ttest/Ttmain.py
+++ Ttest/Ttmain.py
@@ -1,6 +1,12 @@
+j=0
 print('Hello')
 I=float('10.1')
 S='tino'
 print(I+1,chr(1101))
 S=S.replace('in', 'onono')
+print(S)
+while j!=-1:
+    j=S.find('o')
+    S=S[:j] + '!' + S[(j+1):]
+    j=S.find('o')
 print(S)
