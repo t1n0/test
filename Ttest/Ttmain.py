367c92ab325aeaee2c5b2114019747071bdd5b84
Tino Mehlmann <tino.mehlmann@gmail.com>
2018-03-10 23:20:30 -0500

    test1

--- Ttest/Ttmain.py
+++ Ttest/Ttmain.py
@@ -2,11 +2,11 @@ j=0
 print('Hello')
 I=float('10.1')
 S='tino'
-print(I+1,chr(1101))
+print(I+1,chr(255))
 S=S.replace('in', 'onono')
-print(S)
 while j!=-1:
     j=S.find('o')
     S=S[:j] + '!' + S[(j+1):]
     j=S.find('o')
 print(S)
+print('World!')
